1c0c01b5274c0660ed8a22b901eb92cf938ec13e
Kelvin Salton do Prado <kelvinpfw@hotmail.com>
2017-03-12 17:26:03 -0300

    Alterações no arquivo main.py

--- Face_Recognition_Class/main.py
+++ Face_Recognition_Class/main.py
@@ -1,18 +1,35 @@
 
 import FaceRecognition as fr
-from FaceRecognition import Algorithms
+from FaceRecognition import Algorithms, Interpolation
 
-# Defines the path to the training folder
-trainPath = "/home/kelvin/Desktop/FaceRecognition/TestesCurvaROC/BASE1/"
+# Define o caminho para a pasta de treinamento
+trainPath = "/home/Desktop/FaceRecognition/BASE1/"
 
-# Defines the path to the test folder
-testPath  = "/home/kelvin/Desktop/FaceRecognition/TestesCurvaROC/TESTE1/"
+# Define o caminho para a pasta de teste
+testPath  = "/home/Desktop/FaceRecognition/TESTE1/"
 
-newFaceRec = fr.FaceRecognition( Algorithms.EIGENFACE )
+# Define qual algoritmo sera utilizado (padrao: EIGENFACES)
+# [Algorithms.EIGENFACES, Algorithms.FISHERFACES, Algorithms.LBPH, Algorithms.SIFT, Algorithms.SURF]
+faceRec = fr.FaceRecognition( Algorithms.EIGENFACE )
 
-newFaceRec.setInterpolation( Interpolation.INTER_CUBIC )
-newFaceRec.setDefaultSize( 100, 100 )
-newFaceRec.train( trainPath )
-newFaceRec.predict( testPath )
-newFaceRec.showResults()
-newFaceRec.save()
+# Define qual metodo de interpolacao sera utilizado (padrao: INTER_CUBIC)
+# [Interpolation.INTER_CUBIC, Interpolation.INTER_NEAREST, Interpolation.INTER_LINEAR, Interpolation.INTER_AREA, Interpolation.INTER_LANCZOS4]
+faceRec.setInterpolation( Interpolation.INTER_CUBIC )
+
+# Define o tamanho padrao para as imagens (padrao 100x100)
+# Neste caso 100x100 (larguraXaltura)
+faceRec.setDefaultSize( 100, 100 )
+
+# Define o caminho da pasta de treinamento
+faceRec.train( trainPath )
+
+# Define o caminho da pasta de teste
+faceRec.predict( testPath )
+
+# Mostra os resultados na tela
+faceRec.showResults()
+
+# Salva os resultados em uma pasta
+# O caminho da pasta pode ser passado como parametro (ex.: faceRec.save('/home/Desktop/FaceRecognition/Results'))
+# Se o parametro estiver vazio a funcao ira criar uma pasta seguindo o padrao: ano_mes_dia_hora_minuto_segundo_algoritmo_parametros
+faceRec.save()
