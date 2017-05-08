602aef3ab5d0bf4c63951869846131c921ded939
Kelvin Salton do Prado <kelvinpfw@hotmail.com>
2017-05-08 15:39:05 -0300

    Adicionado um exemplo de uso

--- Python/FaceRecognition/main.py
+++ Python/FaceRecognition/main.py
@@ -1,67 +1,39 @@
 
 import os
+import cv2
+import sys
 
-import FaceRecognition as fr
-from Algorithms import Algorithms
-from Interpolation import Interpolation
-from Voting import Voting
+sys.path.append('Auxiliary')
+from Auxiliary import Auxiliary
 
-# Define the path to the training files/folder
-trainPath = "/Users/kelvinsp/Desktop/Treinamento6/"
+sys.path.append('FaceRecognition')
+from FaceRecognition import FaceRecognition
 
-# Define the path to the test folder
-testPath = "/Users/kelvinsp/Desktop/Teste6/"
+sys.path.append('Algorithms')
+from Eigenfaces import Eigenfaces
+from Fisherfaces import Fisherfaces
+from LBPH import LBPH
+from SIFT import SIFT
+from SURF import SURF
 
-faceRec = fr.FaceRecognition()
+def main():
+    # Define the path to the training files/folder
+    trainPath = "/Users/kelvinsp/Desktop/Treinamento6/"
 
-# Set the interpolation method (default: INTER_CUBIC)
-faceRec.setInterpolation(Interpolation.INTER_CUBIC)
+    # Define the path to the test folder
+    testPath = "/Users/kelvinsp/Desktop/Teste6/"
 
-# Set a fixed size for the images (default 100x100)
-faceRec.setDefaultSize(100, 100)
+    auxiliary = Auxiliary(sizeX=100, sizeY=100, interpolation=cv2.INTER_CUBIC)
+    algorithm = Eigenfaces() # numComponents=0, threshold=-1
 
-# Train the algorithms
-faceRec.train(trainPath)
+    faceRecog = FaceRecognition(algorithm, auxiliary)
+    faceRecog.train(trainPath)
+    faceRecog.recognizeFaces(testPath)
+    nonFaces, recognized, unrecognized = faceRecog.getResults()
 
-# In the testPath folder search for all files in all directories
-for dirname, dirnames, filenames in os.walk(testPath):
-    # For each file found
-    for filename in filenames:
+    print "nonFaces" + str(nonFaces)
+    print "recognized" + str(recognized)
+    print "unrecognized" + str(unrecognized)
 
-            # Creates the filePath joining the directory name with the file name
-            filePath = os.path.join(dirname, filename)
-
-            # Try to predict for the current file
-            subjects = faceRec.predict(filePath)
-
-            # If subjects is not None, print the results
-            if subjects is not None:
-                # Store the expected subjectID
-                expectedSubjectID = filename.split("_")[1]
-                expectedSubjectID = int(expectedSubjectID.split(".")[0])
-
-                weights = [1, 1, 1, 1, 1]
-
-                voting = Voting()
-
-                vote1 = voting.majorityVoting(subjects)
-                vote2 = voting.weightedVoting(subjects, weights)
-
-                print filePath
-                print subjects
-
-                if vote1 == expectedSubjectID:
-                    print "Vote 1: " + str(vote1) + " - Correct"
-                else:
-                    print "Vote 1: " + str(vote1) + " - Incorrect"
-
-                if vote2 == expectedSubjectID:
-                    print "Vote 2: " + str(vote2) + " - Correct"
-                else:
-                    print "Vote 2: " + str(vote2) + " - Incorrect"
-
-            else:
-                print "Subjects none"
-
-# Save the result in a text file
-faceRec.save()
+if __name__ == "__main__":
+    main()
