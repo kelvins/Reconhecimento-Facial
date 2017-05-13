3333654ed3bcfc29909ed92689f0f068770ad75c
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-05-13 02:56:38 -0300

    Foi adicionado um script para rodar todos os testes de uma so vez

---
+++ Python/FaceRecognition/Tests/TestRunner.py
@@ -0,0 +1,15 @@
+import glob
+import unittest
+
+# Get all files finished with Test.py
+testFiles = glob.glob('*Test.py')
+
+# Get a list with file name (without the extension .py)
+fileNames = [testFile[0:len(testFile)-3] for testFile in testFiles]
+
+# Create the suites
+suites = [unittest.defaultTestLoader.loadTestsFromName(fileName) for fileName in fileNames]
+
+# Create and run the suite
+suite = unittest.TestSuite(suites)
+unittest.TextTestRunner().run(suite)
