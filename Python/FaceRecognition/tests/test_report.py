5d718e287c6040e85fd36cc18fb8b5ce3583ecda
Kelvin Salton do Prado <kelvinpfw@hotmail.com>
2017-05-16 11:19:53 -0300

    Criação do arquivo de teste da classe Report

---
+++ Python/FaceRecognition/tests/test_report.py
@@ -0,0 +1,21 @@
+import sys
+import unittest
+
+sys.path.append('../classes')
+sys.path.append('../algorithms')
+from report import Report
+from auxiliary import Auxiliary
+from face_recognition import FaceRecognition
+from eigenfaces import Eigenfaces
+
+auxiliary = Auxiliary()
+algorithm = Eigenfaces()
+faceRecog = FaceRecognition(algorithm, auxiliary)
+report    = Report(faceRecog)
+
+class ReportSummaryTest(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(report.generateReportSummary(), "")
+
+if __name__ == '__main__':
+    unittest.main()
