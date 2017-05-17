7a231f2cd673c4b28fefe9c71f458d268ce8bec8
Kelvin Salton do Prado <kelvinpfw@hotmail.com>
2017-05-17 11:09:23 -0300

    Rodar todos os algoritmos, com todas as bases de treinamentos e em todas as bases de testes de uma só vez.

--- Python/FaceRecognition/main.py
+++ Python/FaceRecognition/main.py
@@ -14,6 +14,7 @@ from algorithms.fisherfaces import Fisherfaces
 from algorithms.lbph import LBPH
 from algorithms.sift import SIFT
 from algorithms.surf import SURF
+
 """
 # Set the paths to train, test and store the results
 realPath = os.path.realpath(__file__)
@@ -29,11 +30,57 @@ testPath = dirPath + "/dataset/test/1/"
 resultsPath = dirPath + "/results/"
 """
 
+algs = [Eigenfaces(), Fisherfaces(), LBPH(), SIFT(), SURF()]
+algNames = ["Eigenfaces", "Fisherfaces", "LBPH", "SIFT", "SURF"]
+
 trainPath = "/home/kelvin/Desktop/FaceRecognition/Train/"
 testPath  = "/home/kelvin/Desktop/FaceRecognition/Test/"
-resultsPath = "/home/kelvin/Desktop/FaceRecognition/Results/Eigenfaces"
+resultsPath = "/home/kelvin/Desktop/FaceRecognition/Results/"
 
 def faceFecognition():
+
+    global trainPath, testPath, resultsPath
+
+    # Algorithms loop
+    for algsIndex in xrange(0, len(algs)):
+        # Train folder loop
+        for trainIndex in xrange(1, 7):
+            # Test folder loop
+            for testIndex in xrange(1, 13):
+
+                # Create the auxiliary object
+                auxiliary = Auxiliary(sizeX=100, sizeY=100, interpolation=cv2.INTER_CUBIC)
+
+                # Create the algorithm object
+                algorithm = algs[algsIndex]
+                #algorithm = Eigenfaces()
+                #algorithm = Fisherfaces()
+                #algorithm = LBPH()
+                #algorithm = SIFT()
+                #algorithm = SURF()
+
+                # Create the face recognition object
+                faceRecog = FaceRecognition(algorithm, auxiliary)
+
+                # Train the algorithm
+                faceRecog.train(trainPath + str(trainIndex) + "/")
+
+                # Try to recognize the faces
+                faceRecog.recognizeFaces(testPath + str(testIndex) + "/")
+
+                # Create the report object
+                report = Report(faceRecog)
+
+                # Print the results
+                report.printResults()
+
+                # Save the report (text file)
+                report.saveReport(resultsPath + algNames[algsIndex] + "/")
+
+                # Save all results (summary, full report and images)
+                #report.saveAllResults(resultsPath)
+
+def machineryCommittee():
 
     global trainPath, testPath, resultsPath
 
@@ -46,23 +93,34 @@ def faceFecognition():
             auxiliary = Auxiliary(sizeX=100, sizeY=100, interpolation=cv2.INTER_CUBIC)
 
             # Create the algorithm object
-            algorithm = Eigenfaces()
-            #algorithm = Fisherfaces()
-            #algorithm = LBPH()
-            #algorithm = SIFT()
-            #algorithm = SURF()
+            algorithms = algs
+            """
+            algorithms = []
+            algorithms.append( Eigenfaces() )
+            algorithms.append( Fisherfaces() )
+            algorithms.append( LBPH() )
+            algorithms.append( SIFT() )
+            algorithms.append( SURF() )
+            """
+
+            # Set the weights based on the algorithms list order
+            #weights = [10, 10, 10, 10, 10]
+
+            # Create the voting object setting the WEIGHTED as the voting scheme
+            #voting = Voting(Voting.WEIGHTED, weights)
+            voting = Voting()
 
             # Create the face recognition object
-            faceRecog = FaceRecognition(algorithm, auxiliary)
+            machineryCommittee = MachineryCommittee(algorithms, auxiliary, voting)
 
             # Train the algorithm
-            faceRecog.train(trainPath + str(trainIndex) + "/")
+            machineryCommittee.train(trainPath + str(trainIndex) + "/")
 
             # Try to recognize the faces
-            faceRecog.recognizeFaces(testPath + str(testIndex) + "/")
+            machineryCommittee.recognizeFaces(testPath + str(testIndex) + "/")
 
             # Create the report object
-            report = Report(faceRecog)
+            report = Report(machineryCommittee)
 
             # Print the results
             report.printResults()
@@ -72,48 +130,6 @@ def faceFecognition():
 
             # Save all results (summary, full report and images)
             #report.saveAllResults(resultsPath)
-
-def machineryCommittee():
-
-    global trainPath, testPath, resultsPath
-
-    # Create the auxiliary object
-    auxiliary = Auxiliary(sizeX=100, sizeY=100, interpolation=cv2.INTER_CUBIC)
-
-    # Create the algorithm object
-    algorithms = []
-    algorithms.append( Eigenfaces() )
-    algorithms.append( Fisherfaces() )
-    algorithms.append( LBPH() )
-    algorithms.append( SIFT() )
-    algorithms.append( SURF() )
-
-    # Set the weights based on the algorithms list order
-    weights = [10, 10, 10, 10, 10]
-
-    # Create the voting object setting the WEIGHTED as the voting scheme
-    voting = Voting(Voting.WEIGHTED, weights)
-
-    # Create the face recognition object
-    machineryCommittee = MachineryCommittee(algorithms, auxiliary, voting)
-
-    # Train the algorithm
-    machineryCommittee.train(trainPath)
-
-    # Try to recognize the faces
-    machineryCommittee.recognizeFaces(testPath)
-
-    # Create the report object
-    report = Report(machineryCommittee)
-
-    # Print the results
-    report.printResults()
-
-    # Save the report (text file)
-    report.saveReport(resultsPath)
-
-    # Save all results (summary, full report and images)
-    #report.saveAllResults(resultsPath)
 
 if __name__ == "__main__":
     faceFecognition()
