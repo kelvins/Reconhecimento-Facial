81cd79163402c61d7709ba7041b3c4e78c25fcbc
KelvinS <kelvinpfw@hotmail.com>
2017-06-01 10:36:11 -0300

    Adicionando o main padrão

--- FaceRecognition/main.py
+++ FaceRecognition/main.py
@@ -1,8 +1,5 @@
-
-import os
 import cv2
 import sys
-import gc
 
 from classes.auxiliary import Auxiliary
 from classes.face_recognition import FaceRecognition
@@ -16,196 +13,84 @@ from algorithms.lbph import LBPH
 from algorithms.sift import SIFT
 from algorithms.surf import SURF
 
-"""
-# Set the paths to train, test and store the results
-realPath = os.path.realpath(__file__)
-dirPath  = os.path.dirname(realPath)
-
-# Define the path to the training files/folder
-trainPath = dirPath + "/dataset/train/1/"
-
-# Define the path to the test folder
-testPath = dirPath + "/dataset/test/1/"
-
-# Define the path to the results folder
-resultsPath = dirPath + "/results/"
-"""
-
-algNames = ["Eigenfaces", "Fisherfaces", "LBPH", "SIFT", "SURF"]
-"""
-initialThreshold = [1200,  400,  20,  2,  2]
-finalThreshold   = [1800, 1000, 140, 80, 80]
-stepThreshold    = [   5,    5,   2,  2,  2]
-"""
-trainPath = "/home/kelvins/Desktop/Reconhecimento-Facial/Python/Dataset/Train/BASE"
-testPath  = "/home/kelvins/Desktop/Reconhecimento-Facial/Python/Dataset/Test/VIDEO"
-resultsPath = "/home/kelvins/Desktop/Reconhecimento-Facial/Python/Dataset/Results/2017052501_Ensembles2"
-
-#LBPH 20 - 140 - 1
-#EIGENFACES 1200 - 1800 - 5
-#FISHERFACES 400 - 1000 - 5
+dirPath     = "/home/kelvins/Desktop/Reconhecimento-Facial/Dataset"
+trainPath   = dirPath + "/Train/BASE1/"
+testPath    = dirPath + "/Test/VIDEO1/"
+resultsPath = dirPath + "/Results/"
 
 def faceFecognition():
-
     global trainPath, testPath, resultsPath
 
-    # Algorithms loop
-    for algsIndex in xrange(0, 5):
-        # Train folder loop
-        for trainIndex in xrange(1, 7):
-            # Test folder loop
-            for testIndex in xrange(1, 13):
-                # For each threshold
-                for threshold in xrange(initialThreshold[algsIndex], finalThreshold[algsIndex]+1, stepThreshold[algsIndex]):
+    # Create the auxiliary object
+    auxiliary = Auxiliary(sizeX=100, sizeY=100, interpolation=cv2.INTER_CUBIC)
 
-                    # Create the auxiliary object
-                    auxiliary = Auxiliary(sizeX=100, sizeY=100, interpolation=cv2.INTER_CUBIC)
+    # Create the algorithm object
+    algorithm = Eigenfaces()
+    #algorithm = Fisherfaces()
+    #algorithm = LBPH()
+    #algorithm = SIFT()
+    #algorithm = SURF()
 
-                    print algNames[algsIndex] + ": Train: " + str(trainIndex) + ": Test: " + str(testIndex)
+    # Create the face recognition object
+    faceRecog = FaceRecognition(algorithm, auxiliary)
 
-                    # Create the algorithm object
-                    if algsIndex == 0:
-                        algorithm = Eigenfaces()
-                    elif algsIndex == 1:
-                        algorithm = Fisherfaces()
-                    elif algsIndex == 2:
-                        algorithm = LBPH()
-                    elif algsIndex == 3:
-                        algorithm = SIFT()
-                    elif algsIndex == 4:
-                        algorithm = SURF()
-                    #algorithm = Eigenfaces()
-                    #algorithm = Fisherfaces()
-                    #algorithm = LBPH()
-                    #algorithm = SIFT()
-                    #algorithm = SURF()
+    # Train the algorithm
+    faceRecog.train(trainPath)
 
-                    # Create the face recognition object
-                    faceRecog = FaceRecognition(algorithm, auxiliary, threshold)
+    # Try to recognize the faces
+    faceRecog.recognizeFaces(testPath)
 
-                    # Train the algorithm
-                    faceRecog.train(trainPath + str(trainIndex) + "/")
+    # Create the report object
+    report = Report(faceRecog)
 
-                    # Try to recognize the faces
-                    faceRecog.recognizeFaces(testPath + str(testIndex) + "/")
+    # Print the results
+    report.printResults()
 
-                    # Create the report object
-                    report = Report(faceRecog)
+    # Save the report (text file)
+    report.saveReport(resultsPath)
 
-                    # Print the results
-                    #report.printResults()
-
-                    # Save the report (text file)
-                    report.saveReport(resultsPath + algNames[algsIndex] + "/" + str(trainIndex) + "/" + str(testIndex) + "/")
-
-                    # Save all results (summary, full report and images)
-                    #report.saveAllResults(resultsPath)
-
-                    del auxiliary
-                    del algorithm
-                    del faceRecog
-                    del report
-
-                    gc.collect()
+    # Save all results (summary, full report and images)
+    #report.saveAllResults(resultsPath)
 
 def ensemble():
-
     global trainPath, testPath, resultsPath
 
-    weights = []
-    # We can't set a higher value
-    # For example: [60, 10, 10, 10, 10]
-    # the result will always be the result of eigenfaces
+    # Create the auxiliary object
+    auxiliary = Auxiliary(sizeX=100, sizeY=100, interpolation=cv2.INTER_CUBIC)
 
-    weights.append([40, 10, 10, 10, 10])
-    weights.append([10, 40, 10, 10, 10])
-    weights.append([10, 10, 40, 10, 10])
-    weights.append([10, 10, 10, 40, 10])
-    weights.append([10, 10, 10, 10, 40])
+    # Create and add all algorithms for the ensemble
+    algorithms = []
+    algorithms.append( Eigenfaces() )
+    algorithms.append( LBPH() )
+    algorithms.append( Fisherfaces() )
+    algorithms.append( SIFT() )
+    algorithms.append( SURF() )
 
-    #print len(weights)
+    # Create the voting object (Majority or Weighted)
+    #voting = Voting()
+    voting = Voting(Voting.WEIGHTED, [10, 20, 30, 30, 40])
 
-    for i in xrange(0, len(weights)):
-        """
-        algs = []
-        algs.append([Eigenfaces(), LBPH(), Fisherfaces()])
-        algs.append([Eigenfaces(), LBPH(), SIFT()])
-        algs.append([Eigenfaces(), LBPH(), SURF()])
-        algs.append([Eigenfaces(), Fisherfaces(), SIFT()])
-        algs.append([Eigenfaces(), Fisherfaces(), SURF()])
-        algs.append([Eigenfaces(), SIFT(), SURF()])
-        algs.append([LBPH(), Fisherfaces(), SIFT()])
-        algs.append([LBPH(), Fisherfaces(), SURF()])
-        algs.append([LBPH(), SIFT(), SURF()])
-        algs.append([Fisherfaces(), Sy  IFT(), SURF()])
-        """
+    # Create the ensemble object
+    ensemble = Ensemble(algorithms, auxiliary, voting)
 
-        #for alg in algs:
-        # Create the auxiliary object
-        auxiliary = Auxiliary(sizeX=100, sizeY=100, interpolation=cv2.INTER_CUBIC)
+    # Train the algorithm
+    ensemble.train(trainPath)
 
-        # Create the algorithm object
-        algorithms = []
-        algorithms.append( Eigenfaces() )
-        algorithms.append( LBPH() )
-        algorithms.append( Fisherfaces() )
-        algorithms.append( SIFT() )
-        algorithms.append( SURF() )
+    # Try to recognize the faces
+    ensemble.recognizeFaces(testPath)
 
-        # Set the weights based on the algorithms list order
-        #weights = [10, 10, 10, 10, 10]
+    # Create the report object
+    report = Report(ensemble)
 
-        # Create the voting object setting the WEIGHTED as the voting scheme
-        voting = Voting(Voting.WEIGHTED, weights[i])
-        #voting = Voting()
+    # Print the results
+    report.printResults()
 
-        # Create the ensemble object
-        ensemble = Ensemble(algorithms, auxiliary, voting)
+    # Save the report (text file)
+    report.saveReport(resultsPath)
 
-        # Train the algorithm
-        ensemble.train(trainPath + "6/")
-
-        # Try to recognize the faces
-        ensemble.recognizeFaces(testPath + "11/")
-
-        # Create the report object
-        report = Report(ensemble)
-
-        # Print the results
-        #report.printResults()
-
-        # Save the report (text file)
-        report.saveReport(resultsPath + "/")
-
-        output = ', '.join(map(str, weights[i])) + " "
-        #for x in xrange(0, len(algorithms)):
-        #    output += algorithms[x].getAlgorithmName() + ", "
-        # Avoid division by zero
-        totalFaceImages = float(ensemble.getRecognized() + ensemble.getUnrecognized())
-        if totalFaceImages > 0.0:
-            accuracy = (float(ensemble.getRecognized()) / totalFaceImages) * 100.0
-            output += "Accuracy: " + "{0:.2f}".format(accuracy) + " %"
-        else:
-            output += "Accuracy: 0.0 %"
-
-        print output
-
-        # Save all results (summary, full report and images)
-        #report.saveAllResults(resultsPath)
-
-        del auxiliary
-        del algorithms
-        del voting
-        del ensemble
-        del report
-
-        del totalFaceImages
-        del output
-        del accuracy
-
-        gc.collect()
-
+    # Save all results (summary, full report and images)
+    #report.saveAllResults(resultsPath)
 
 if __name__ == "__main__":
     faceFecognition()
-    #ensemble()
+    ensemble()
