e2b39c28ceb32bf6dd5f37eef68309bb80bbafaa
KelvinS <kelvinpfw@hotmail.com>
2017-09-03 17:08:48 -0300

    Correções nos testes da classe auxiliary.

--- FaceRecognition/tests/test_auxiliary.py
+++ FaceRecognition/tests/test_auxiliary.py
@@ -10,99 +10,99 @@ auxiliary = Auxiliary()
 class GetInterpolationMethodNameTest(unittest.TestCase):
     def test1(self):
         self.assertEqual(
-            auxiliary.getInterpolationMethodName(),
+            auxiliary.get_interpolation_method_name(),
             "cv2.INTER_CUBIC")
 
     def test2(self):
         auxiliary.interpolation = cv2.INTER_LANCZOS4
         self.assertEqual(
-            auxiliary.getInterpolationMethodName(),
+            auxiliary.get_interpolation_method_name(),
             "cv2.INTER_LANCZOS4")
 
     def test2(self):
-        auxiliary.setInterpolation(123)
-        self.assertEqual(auxiliary.getInterpolationMethodName(), "")
+        auxiliary.interpolation = 123
+        self.assertEqual(auxiliary.get_interpolation_method_name(), "")
 
 
 class GetSupportedFilesTest(unittest.TestCase):
     def test1(self):
-        self.assertEqual(auxiliary.supportedFiles, ["png", "jpg", "jpeg"])
+        self.assertEqual(auxiliary.supported_files, ["png", "jpg", "jpeg"])
 
     def test2(self):
-        auxiliary.supportedFiles = ["png", "jpg", "jpeg", "gif"]
+        auxiliary.supported_files = ["png", "jpg", "jpeg", "gif"]
         self.assertEqual(
-            auxiliary.supportedFiles, ["png", "jpg", "jpeg", "gif"])
+            auxiliary.supported_files, ["png", "jpg", "jpeg", "gif"])
 
 
 class GetDefaultSizeTest(unittest.TestCase):
     def test1(self):
-        self.assertEqual(auxiliary.getDefaultSize(), (100, 100))
+        self.assertEqual(auxiliary.get_default_size(), (100, 100))
 
     def test2(self):
-        auxiliary.setDefaultSize(200, 200)
-        self.assertEqual(auxiliary.getDefaultSize(), (200, 200))
+        auxiliary.set_default_size(200, 200)
+        self.assertEqual(auxiliary.get_default_size(), (200, 200))
 
     def test3(self):
-        auxiliary.setDefaultSize(-5, -5)
-        self.assertEqual(auxiliary.getDefaultSize(), (200, 200))
+        auxiliary.set_default_size(-5, -5)
+        self.assertEqual(auxiliary.get_default_size(), (200, 200))
 
     def test4(self):
-        auxiliary.setDefaultSize(-5, 100)
-        self.assertEqual(auxiliary.getDefaultSize(), (200, 100))
+        auxiliary.set_default_size(-5, 100)
+        self.assertEqual(auxiliary.get_default_size(), (200, 100))
 
     def test5(self):
-        auxiliary.setDefaultSize(50, -5)
-        self.assertEqual(auxiliary.getDefaultSize(), (50, 100))
+        auxiliary.set_default_size(50, -5)
+        self.assertEqual(auxiliary.get_default_size(), (50, 100))
 
 
 class GrayscaleTest(unittest.TestCase):
     def test1(self):
-        img = auxiliary.loadImage("images/python.png")
-        self.assertEqual(auxiliary.isGrayscale(img), False)
+        img = auxiliary.load_image("images/python.png")
+        self.assertEqual(auxiliary.is_grayscale(img), False)
 
     def test2(self):
-        img = auxiliary.loadImage("images/python.png")
-        img = auxiliary.toGrayscale(img)
-        auxiliary.saveImage("images/python_gray.png", img)
-        self.assertEqual(auxiliary.isGrayscale(img), True)
+        img = auxiliary.load_image("images/python.png")
+        img = auxiliary.to_grayscale(img)
+        auxiliary.save_image("images/python_gray.png", img)
+        self.assertEqual(auxiliary.is_grayscale(img), True)
 
     def test3(self):
-        img = auxiliary.loadImage("images/python_gray.png")
-        self.assertEqual(auxiliary.isGrayscale(img), True)
+        img = auxiliary.load_image("images/python_gray.png")
+        self.assertEqual(auxiliary.is_grayscale(img), True)
 
 
 class PreprocessImageTest(unittest.TestCase):
     def test1(self):
-        img = auxiliary.loadImage("images/python.png")
-        img = auxiliary.resizeImage(img, 400, 400, cv2.INTER_CUBIC)
+        img = auxiliary.load_image("images/python.png")
+        img = auxiliary.resize_image(img, 400, 400, cv2.INTER_CUBIC)
         self.assertEqual(img.shape[:2], (400, 400))
 
     def test2(self):
-        auxiliary.setDefaultSize(100, 100)
+        auxiliary.set_default_size(100, 100)
         auxiliary.interpolation = cv2.INTER_CUBIC
-        img = auxiliary.preprocessImage("images/python.png")
+        img = auxiliary.preprocess_image("images/python.png")
         self.assertEqual(img.shape[:2], (100, 100))
-        self.assertEqual(auxiliary.isGrayscale(img), True)
+        self.assertEqual(auxiliary.is_grayscale(img), True)
 
 
 class ConcatenateImagesTest(unittest.TestCase):
     def test1(self):
-        img1 = auxiliary.loadImage("images/python.png")
-        img1 = auxiliary.resizeImage(img1, 100, 100, cv2.INTER_CUBIC)
+        img1 = auxiliary.load_image("images/python.png")
+        img1 = auxiliary.resize_image(img1, 100, 100, cv2.INTER_CUBIC)
 
-        img2 = auxiliary.loadImage("images/python_gray.png")
-        img2 = auxiliary.resizeImage(img2, 100, 100, cv2.INTER_CUBIC)
+        img2 = auxiliary.load_image("images/python_gray.png")
+        img2 = auxiliary.resize_image(img2, 100, 100, cv2.INTER_CUBIC)
 
-        img = auxiliary.concatenateImages(img1, img2)
-        auxiliary.saveImage("images/python_concatenated.png", img)
+        img = auxiliary.concatenate_images(img1, img2)
+        auxiliary.save_image("images/python_concatenated.png", img)
 
-        img = auxiliary.loadImage("images/python_concatenated.png")
+        img = auxiliary.load_image("images/python_concatenated.png")
         self.assertEqual(img.shape[:2], (100, 200))
 
 
 class ExtractPathsTest(unittest.TestCase):
     def test1(self):
-        paths = auxiliary.extractImagesPaths("images/")
+        paths = auxiliary.extract_images_paths("images/")
         self.assertEqual(len(paths), 3)
 
 
@@ -110,9 +110,9 @@ class WriteTextFileTest(unittest.TestCase):
     def test1(self):
         content = "WriteTextFileTest"
         fileName = "images/WriteTextFileTest.txt"
-        auxiliary.writeTextFile(content, fileName)
+        auxiliary.write_text_file(content, fileName)
 
-        paths = auxiliary.extractFilesPaths("images/")
+        paths = auxiliary.extract_files_paths("images/")
         self.assertEqual(len(paths), 4)
 
 
